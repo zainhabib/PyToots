36d00f40aec2f4090d96821cb94a3b7da5c9c82c
unknown <zain.n.habib@gmail.com>
2023-05-27 15:16:44 -0500

    First Commit

---
+++ hello.py
@@ -0,0 +1,31 @@
+print("Hello, World!")
+
+x=12
+# Print out value of x, type of x
+print(f"x = {x}, type={type(x)}") # int=Integer
+
+x=12.57
+print(f"x = {x}, type={type(x)}") # float=float
+
+x="Braeden and Isaac"
+print(f"x = {x}, type={type(x)}") # str=String
+
+x=False
+print(f"x = {x}, type={type(x)}") # bool=boolean
+
+x=4/2
+print(f"x = {x}, type={type(x)}") # division answer is always a float
+
+x=2*3
+print(f"x = {x}, type={type(x)}") # Multiplication of two ints will be an int
+
+x=1.1 * 2
+print(f"x = {x}, type={type(x)}") # Multiplication of at least one flot will result in float
+
+x=2+7
+print(f"x = {x}, type={type(x)}") # same as multiplication
+
+x=3.4+1
+print(f"x = {x}, type={type(x)}") # same as multiplication
+
+
