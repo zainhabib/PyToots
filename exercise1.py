42cc372fe0a16c96f86e9b80a100ec4910e70a29
zainhabib <zain.n.habib@gmail.com>
2023-05-31 15:58:41 -0500

    Circle Area

---
+++ exercise1.py
@@ -0,0 +1,11 @@
+from math import pi
+
+name = input("Please type your name:")
+print(f"Nice to meet you, {name}")
+
+r = input("Please type in the radius")
+print(f"r (radius) is {r}")
+
+
+
+print(pi)
